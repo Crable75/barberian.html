7b6dd98c4e9adada437c40eb5537c37a37376f64
Crable <vladimir.ischuk@gmail.com>
2019-10-13 20:13:57 +0300

    homepage tuning - working

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,54 @@
 $(function()
 {
-    $('.header__menu-btn').on('click', function () {
-        $('.header').toggleClass('header--show');
-    });
+  // Preloader
+  $(window).on('load', function() {
+    $('body').addClass('loaded');
+  });
+
+  $('.header__menu-btn').on('click', function () {
+    $('.header').toggleClass('header--show');
+  });
+
+  $('.history__slider').slick({
+    vertical: true,
+    // verticalSwiping: true,
+    dots: false,
+    arrows: true,
+    slidesToShow: 4,
+    infinite: true,
+    prevArrow: '.history__slider-up',
+    nextArrow: '.history__slider-down',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          vertical: false,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          vertical: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          vertical: false,
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+
+  $('.events__slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 1,
+    infinite: true,
+    prevArrow: '.events__slider-left',
+    nextArrow: '.events__slider-right'
+  });
 
 });
